de96ed4cec64911f010629e10f48b2206c886d56
te19tsukinoki <te19tsukinoki@g.kumamoto-nct.ac.jp>
2023-03-11 13:06:32 +0900

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public gs: GlobalService
+  ) {}
+  instagram_url: string = '';
+  SampleTweet: string = 'ここにツイート内容が表示されます';
+  send: any ={};
+  botten = true;
+  errorStatus = 0;
+  
+  conversion = () => {
+    console.log(this.instagram_url);
+    this.SampleTweet = "power is power"
+  }
+
+  urlCheck() {
+    //console.log(this.errorStatus);
+    if(this.instagram_url.includes("https://www.instagram.com/p/")){
+      this.botten = false;
+      this.errorStatus = 1;
+    }else{
+      this.botten = true;
+      this.errorStatus = -1;
+    }
+    
+  }
 
 }
