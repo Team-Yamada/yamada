6ed9c6630393627411c4e7220a635d6117339881
te19tsukinoki <te19tsukinoki@g.kumamoto-nct.ac.jp>
2023-03-11 17:45:49 +0900

    twitter add

--- src/app/twitter/twitter.page.ts
+++ src/app/twitter/twitter.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-twitter',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TwitterPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public gs: GlobalService
+  ) {}
+
+  SampleTweet: string = 'ここにツイート内容が表示されます';
+  send: any ={};
+  botten = true;
+  
+  customCounterFormatter(inputLength: number, maxLength: number) {
+    return `残り${maxLength - inputLength}文字です。 `;
+  }
 
   ngOnInit() {
   }
